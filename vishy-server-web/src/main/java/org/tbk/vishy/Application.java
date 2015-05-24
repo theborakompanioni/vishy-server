c8433146bd7cafeb29afeb036b0486ea34409593
theborakompanioni <theborakompanioni+github@gmail.com>
2015-05-24 23:03:59 +0200

    add static resource handler

--- vishy-server-web/src/main/java/org/tbk/vishy/Application.java
+++ vishy-server-web/src/main/java/org/tbk/vishy/Application.java
@@ -1,5 +1,7 @@
 package org.tbk.vishy;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -9,10 +11,12 @@ import java.util.Arrays;
 @SpringBootApplication
 public class Application {
 
+    private static Log logger = LogFactory.getLog(Application.class);
+
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(Application.class, args);
 
-        System.out.println("Let's inspect the beans provided by Spring Boot:");
+        logger.info("Beans in application context:");
 
         String[] beanNames = ctx.getBeanDefinitionNames();
         Arrays.sort(beanNames);
---
+++ vishy-server-web/src/main/java/org/tbk/vishy/config/StaticsResourcesConfiguration.java
@@ -0,0 +1,49 @@
+package org.tbk.vishy.config;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.util.Arrays;
+
+/**
+ * Created by void on 24.05.15.
+ */
+@Configuration
+public class StaticsResourcesConfiguration extends WebMvcAutoConfiguration.WebMvcAutoConfigurationAdapter {
+
+    private static Log logger = LogFactory.getLog(StaticsResourcesConfiguration.class);
+
+    private static final int CACHE_PERIOD = 1000;
+
+    private static final String[] SERVLET_RESOURCE_LOCATIONS = { "/" };
+
+    private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
+        "classpath:/static/", "classpath:/public/"
+    };
+
+    private static final String[] RESOURCE_LOCATIONS;
+    static {
+        RESOURCE_LOCATIONS = new String[CLASSPATH_RESOURCE_LOCATIONS.length
+                + SERVLET_RESOURCE_LOCATIONS.length];
+        System.arraycopy(SERVLET_RESOURCE_LOCATIONS, 0, RESOURCE_LOCATIONS, 0,
+                SERVLET_RESOURCE_LOCATIONS.length);
+        System.arraycopy(CLASSPATH_RESOURCE_LOCATIONS, 0, RESOURCE_LOCATIONS,
+                SERVLET_RESOURCE_LOCATIONS.length, CLASSPATH_RESOURCE_LOCATIONS.length);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        super.addResourceHandlers(registry);
+
+        logger.info("Add resource handler for 'static/**' to " + Arrays.toString(RESOURCE_LOCATIONS));
+
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations(RESOURCE_LOCATIONS)
+                .setCachePeriod(CACHE_PERIOD);
+
+    }
+}
