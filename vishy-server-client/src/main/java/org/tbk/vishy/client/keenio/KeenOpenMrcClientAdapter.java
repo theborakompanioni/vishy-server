4baa497ab72f258cad39f7e1acf1f6908f50dc01
theborakompanioni <theborakompanioni+github@gmail.com>
2015-06-30 19:20:45 +0200

    keenio: provide a function to extract event name from request

--- vishy-server-client/src/main/java/org/tbk/vishy/client/keenio/KeenOpenMrcClientAdapter.java
+++ vishy-server-client/src/main/java/org/tbk/vishy/client/keenio/KeenOpenMrcClientAdapter.java
@@ -3,6 +3,7 @@ package org.tbk.vishy.client.keenio;
 import com.google.common.collect.Maps;
 import io.keen.client.java.KeenCallback;
 import io.keen.client.java.KeenClient;
+import io.keen.client.java.KeenProject;
 import lombok.extern.slf4j.Slf4j;
 import org.tbk.openmrc.OpenMrc;
 import org.tbk.openmrc.OpenMrcRequestConsumer;
@@ -10,6 +11,7 @@ import org.tbk.vishy.client.OpenMrcRequestToMapFunction;
 
 import java.util.Map;
 import java.util.Optional;
+import java.util.function.Function;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
@@ -20,45 +22,56 @@ import static java.util.Map.Entry;
  */
 @Slf4j
 public class KeenOpenMrcClientAdapter implements OpenMrcRequestConsumer {
+    private static Function<OpenMrc.Request, String> DEFAULT_EVENT_NAME_FUNC = request -> request.getType().name();
+
+    private static KeenProject NULL_KEEN_PROJECT = null;
+    private static Map<String, Object> NULL_KEEN_PROPERTIES = null;
+    private static KeenCallback DEFAULT_KEEN_CALLBACK = new KeenCallback() {
+        @Override
+        public void onSuccess() {
+        }
+
+        @Override
+        public void onFailure(Exception e) {
+            log.warn("", e);
+        }
+    };
 
     private final KeenClient keenClient;
     private final OpenMrcRequestToMapFunction toMapFunction;
+    private final Function<OpenMrc.Request, String> toEventNameFunction;
 
     public KeenOpenMrcClientAdapter(KeenClient keenClient, OpenMrcRequestToMapFunction toMapFunction) {
+        this(keenClient, toMapFunction, DEFAULT_EVENT_NAME_FUNC);
+    }
+
+    public KeenOpenMrcClientAdapter(KeenClient keenClient, OpenMrcRequestToMapFunction toMapFunction, Function<OpenMrc.Request, String> toEventNameFunction) {
         this.keenClient = keenClient;
         this.toMapFunction = toMapFunction;
+        this.toEventNameFunction = toEventNameFunction;
     }
 
     @Override
     public void accept(OpenMrc.Request request) {
         Map<String, Object> event = Optional.ofNullable(request)
                 .map(toMapFunction)
+                .map(this::replaceDotsInKeysWithColon)
                 .orElseThrow(IllegalArgumentException::new);
 
-        Map<String, Object> cleanedEvent = replaceDotsInKeysWithColon(event);
+        String eventName = toEventNameFunction.apply(request);
 
-        log.info("received event for keenio {}", request.getType());
+        log.debug("received event for keenio {}", eventName);
 
-        keenClient.addEvent(null, request.getType().name(), cleanedEvent, null, new KeenCallback() {
-            @Override
-            public void onSuccess() {
-                log.debug("successfully sent keenio event: {}", cleanedEvent);
-            }
-
-            @Override
-            public void onFailure(Exception e) {
-                log.warn("", e);
-            }
-        });
+        keenClient.addEvent(NULL_KEEN_PROJECT, eventName, event, NULL_KEEN_PROPERTIES, DEFAULT_KEEN_CALLBACK);
     }
 
     // keen does not like certain events
     // e.g. with "dots" in the key
     private <V> Map<String, V> replaceDotsInKeysWithColon(Map<String, V> event) {
-        return newTransformedMap(event, Collectors.toMap(e -> e.getKey().replace(".", ":"), Entry::getValue));
+        return newMapFromEntries(event, Collectors.toMap(e -> e.getKey().replace(".", ":"), Entry::getValue));
     }
 
-    private <K, V> Map<K, V> newTransformedMap(Map<K, V> target, Collector<Entry<K, V>, ?, Map<K, V>> collector) {
+    private <K, V> Map<K, V> newMapFromEntries(Map<K, V> target, Collector<Entry<K, V>, ?, Map<K, V>> collector) {
         Map<K, V> replacedKeysMap = Maps.newHashMapWithExpectedSize(target.size());
 
         replacedKeysMap.putAll(target.entrySet().stream()
