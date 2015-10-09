5786914b57d2bc14810131fa929689fea64cdee3
theborakompanioni <theborakompanioni+github@gmail.com>
2015-10-09 19:25:18 +0200

    write pid and port in files on startup

--- vishy-server-web/src/main/java/org/tbk/vishy/Application.java
+++ vishy-server-web/src/main/java/org/tbk/vishy/Application.java
@@ -1,8 +1,11 @@
 package org.tbk.vishy;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.actuate.system.ApplicationPidFileWriter;
+import org.springframework.boot.actuate.system.EmbeddedServerPortFileWriter;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationListener;
 
 @Slf4j
 @SpringBootApplication
@@ -12,8 +15,18 @@ public class Application {
         new SpringApplicationBuilder()
                 .sources(Application.class)
                 .showBanner(true)
+                .listeners(applicationPidFileWriter(), embeddedServerPortFileWriter())
                 .web(true)
                 .run(args);
     }
+
+    public static ApplicationListener<?> applicationPidFileWriter() {
+        return new ApplicationPidFileWriter("app.pid");
+    }
+
+    public static ApplicationListener<?> embeddedServerPortFileWriter() {
+        return new EmbeddedServerPortFileWriter("app.port");
+    }
+
 
 }
