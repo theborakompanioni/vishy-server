b30b29b5bedc0064c8cf1eb6977f837bbeff7bd1
theborakompanioni <theborakompanioni+github@gmail.com>
2016-11-10 00:11:45 +0100

    externalize module vishy-server-client

--- vishy-server-client/src/main/java/org/tbk/vishy/client/OpenMrcRequestToMapFunction.java
+++
@@ -1,9 +0,0 @@
-package org.tbk.vishy.client;
-
-import com.github.theborakompanioni.openmrc.OpenMrc;
-
-import java.util.Map;
-import java.util.function.Function;
-
-public interface OpenMrcRequestToMapFunction extends Function<OpenMrc.Request, Map<String, Object>> {
-}
